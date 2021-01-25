f1cf4addd48e35f52087cfa20c535c02c72cc76d
Jupiter <jupiter.programmer@gmail.com>
2021-01-25 06:00:43 +0400

    basic strcuture

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { useEffect } from "react";
+
+function App() {
+  useEffect(() => {
+    /**
+     * YOUR ONE SIGNAL CONFIGURATION GOES HERE
+     */
+  }, []);
+
+  return (
+    <>
+      <h1>One Signal - React DEMO</h1>
+    </>
+  );
+}
+
+export default App;
